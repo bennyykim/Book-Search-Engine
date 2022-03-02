92c1482b95a3adf0eb8df3ff6b03fcd6abf62457
Benjamin Kim <bennykim523@gmail.com>
2022-03-01 23:17:24 -0500

    added mutations

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,45 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+        token
+        user {
+            _id
+            username
+        }
+    }
+}
+`;
+
+export const ADD_USER = gql`
+mutation createUser($username: String!, $email: String!, $password: String!) {
+    createUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+mutation saveBook($bookId: ID!) {
+    saveBook(bookId: $bookId) {
+        _id
+        authors
+        description
+        title
+        image
+    }
+}
+`;
+
+export const REMOVE_BOOK = gql`
+    mutation removeBook($bookId: String!) {
+        removeBook(bookId: $bookId) {
+            _id
+        }
+    }
+`
