baa9926db2caab207a3d1c85ab9f21d693ef188c
Benjamin Kim <bennykim523@gmail.com>
2022-03-01 21:49:32 -0500

    added queries and mutations

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,7 +1,40 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-    
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]!
+    }
+
+    type Book {
+        authors: [String]!
+        description: String
+        bookId: String
+        image: String
+        link: String
+        title: String
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+
+    type Query {
+        users: [User]
+        books: [Book]
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!) : Auth
+        addUser(username: String!, email: String!, password: String!) : Auth
+        saveBook(authors: [String]!, description: String!, title: String!, bookId: String!, image: String!, link: String!) : User
+        removeBook(bookId: String!) : User
+    }
 `;
 
 module.exports = typeDefs;
